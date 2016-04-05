ce47f66d7740aabe0d6430423a9332f878d2cd82
Veleri <R.Veleri@yandex.ua>
2016-04-05 16:55:35 +0300

    Add demo book

---
+++ src/main/java/model/Book.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Created by Фокстрот on 02.04.2016.
+ */
+public class Book extends Item{
+
+    public Book(int id, String name, String des, int par, ItemType itemType) {
+        super(id, name, des, par, itemType);
+    }
+}
