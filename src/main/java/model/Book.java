3b905a044be5977889aec7c1d96d3d978c54ba92
Veleri <R.Veleri@yandex.ua>
2016-04-05 17:50:51 +0300

    Update classes Book and Item.

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -1,11 +1,57 @@
 package model;
 
+
 /**
- * Created by Фокстрот on 02.04.2016.
+ * Class that describes the book's.
+ *
+ * @author Veleri Rechembei
+ * @version %I%, %G%
  */
 public class Book extends Item{
 
-    public Book(int id, String name, String des, int par, ItemType itemType) {
-        super(id, name, des, par, itemType);
+    private int idAuthor;
+    private int pages;
+    private int price;
+    private int amount;
+
+    public Book(int id, String name, String des, int par, int idAuthor, int pages, int price, int amount) {
+        super(id, name, des, par, ItemType.Book);
+        this.idAuthor=idAuthor;
+        this.pages=pages;
+        this.price=price;
+        this.amount=amount;
     }
+
+    public int getIdAuthor() {
+        return idAuthor;
+    }
+
+    public void setIdAuthor(int idAuthor) {
+        this.idAuthor = idAuthor;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
 }
--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -1,14 +1,18 @@
 package model;
 
 /**
- * Created by Фокстрот on 02.04.2016.
+ * Class that describes the item's.
+ *
+ * @author Veleri Rechembei
+ * @version %I%, %G%
  */
 public class Item {
-    private int idItem;
+    private final int idItem;
     private String name;
     private String description;
     private int parent;
     private ItemType type;
+
 
     public Item(int id, String name, String des,int par,ItemType itemType){
         this.idItem=id;
@@ -19,7 +23,44 @@ public class Item {
     }
     public enum ItemType {
         Rubric,
+        Book,
         Section;
     }
+    public Item(int id ){
+        this.idItem=id;
+    }
+    public ItemType getType() {
+        return type;
+    }
+
+    public void setType(ItemType type) {
+        this.type = type;
+    }
+
+    public int getParent() {
+        return parent;
+    }
+
+    public void setParent(int parent) {
+        this.parent = parent;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 
 }
