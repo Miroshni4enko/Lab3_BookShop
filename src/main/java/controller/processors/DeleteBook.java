6851ad1e2d14e6c2c108b6ccd75a2ae656a275d4
Slavik Miroshnishenko <miroshni4encko.slavikkk@yandex.ru>
2016-05-20 14:50:43 +0300

    fix bag orders

--- src/main/java/controller/processors/DeleteBook.java
+++ src/main/java/controller/processors/DeleteBook.java
@@ -2,11 +2,14 @@ package controller.processors;
 
 import Servlet.Commands;
 import exception.DataBaseException;
+import model.Customer;
 import model.OracleDataAccess;
+import model.Order;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class for delete book.
@@ -22,6 +25,15 @@ public class DeleteBook implements GeneralProcess {
         OracleDataAccess.getInstance().removeBook(idBook);
         ArrayList books = (ArrayList) OracleDataAccess.getInstance().getAmountOfBooks(Commands.AMOUNT_OF_BOOKS_ON_LIST);
         request.getSession().setAttribute(ViewListBooks.ATTRIBUTE_LIST_OF_ALL_BOOKS, books);
+        List<Order> listOrders;
+        Customer customer  = (Customer) request.getSession().getAttribute(LoginUser.ATTRIBUTE_CUSTOMER);
+        if(customer.getRole()==10) {
+            listOrders = OracleDataAccess.getInstance().getAllOrder();
+        }else {
+            listOrders = OracleDataAccess.getInstance().getOrderByIdCustomer(customer.getId());
+        }
+        request.getSession().setAttribute(UpdateOrder.LIST_ORDERS, null);
+        request.getSession().setAttribute(UpdateOrder.LIST_ORDERS, listOrders);
 
         Commands.forward("/index.jsp", request, response);
     }
